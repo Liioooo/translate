d239d52c5ac6315e7e5c4e86bd15e7257085db9c
Stefan Mikulik <6148@htl.rennweg.at>
2019-10-08 13:26:04 +0200

    implement translation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TranslationService} from '../services/translation.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    public textIn: string;
+    public textOut: string;
+    public languages: { lang: string, short: string }[];
+    private from = 'en';
+    private to = 'de';
+
+    constructor(private translationService: TranslationService) {
+        this.translationService.getLanguages().subscribe(res => this.languages = res);
+    }
+
+    public translate(): void {
+        this.translationService.getTranslation(this.textIn, this.from, this.to).subscribe(res => this.textOut = res);
+    }
+
+    public switchLanguages() {
+        const newFrom = this.to;
+        this.to = this.from;
+        this.from = newFrom;
+        const newOut = this.textIn;
+        this.textIn = this.textOut;
+        this.textOut = newOut;
+    }
+
 
 }
